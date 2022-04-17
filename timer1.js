07a7a8e3c65f80a4ef6a365e02b392fd2684b790
Billy Wong <bwong.yh@protonmail.com>
2022-04-17 21:21:38 +0900

    Update used map func to create new array. forEach is now responsible for setTimeout ONLY

--- timer1.js
+++ timer1.js
@@ -1,8 +1,10 @@
 const args = process.argv;
-const ringTimes = args.slice(2).filter(time => Number(time) >= 0);
+const ringTimes = args
+  .slice(2)
+  .filter(time => Number(time) >= 0)
+  .map(time => Number(time) * 1000);
 
 ringTimes.forEach(time => {
-  time = Number(time) * 1000;
   setTimeout(() => {
     process.stdout.write("\x07");
   }, time);
